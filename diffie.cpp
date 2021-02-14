5910bd1637cbcf50e20c2f53d922b246c6d24b78
Saad Noor <ssaadn.555@gmail.com>
2021-02-14 14:55:38 +0600

    diffie-hallman algo

--- diffie.cpp
+++ diffie.cpp
@@ -21,13 +21,32 @@ ll pow_a_b(ll a, ll b, ll mod)
 
 int main()
 {
-    ll xa, ya, xb, yb, alfa= 3, ka, kb;     
+    ll xa, ya, xb, yb, alfa= 9, ka, kb;     
     /*  
         xa, xb-> private keys for a, b
         ya, yb-> public keys for a,b
         ka, kb-> calculated secret key
         alfa-> premitive root of the given prime number
     */
+
+    input:
+    cout<< "Enter value of alfa: ";
+    cin>> alfa;
+
+    bool prr[prm];
+    for(int i=0; i<prm; i++) prr[i]= 0;
+    for(int i=0; i<prm-1; i++)
+        prr[pow_a_b(alfa, i, prm)]=1;
+
+    for(int i=1; i<prm; i++) 
+        if(!prr[i]) 
+        {
+            cout<< i<< '\n';
+            cout<< "alfa is not a premitive root of "<< prm<< '\n';
+            // return 0;
+            goto input;
+        }
+
     cout<< "Enter chosen private key for A: ";
     cin>> xa;
     cout<< "Enter chosen private key for B: ";
